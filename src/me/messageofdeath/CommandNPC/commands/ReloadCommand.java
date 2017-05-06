23f14c31494d8d2dcfb1cff6e0acfbaaba5fc2c9
BuildTools <unconfigured@null.spigotmc.org>
2017-05-06 06:06:49 -0500

    Fixed bug when you only typed the command it would send a NullPointer

--- src/me/messageofdeath/CommandNPC/commands/ReloadCommand.java
+++ src/me/messageofdeath/CommandNPC/commands/ReloadCommand.java
@@ -12,10 +12,20 @@ public class ReloadCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (args[0].equalsIgnoreCase("reload") && (sender.hasPermission("commandnpc.admin") || sender.isOp())) {
-			CommandNPC.getConfigX().reloadConfig();
-			Messaging.send(sender, "Reloaded CommandNPC");
-			return true;
+		if(args.length == 1) {
+			if (args[0].equalsIgnoreCase("reload")) {
+				if(sender.hasPermission("commandnpc.admin") || sender.isOp()) {
+					CommandNPC.getConfigX().reloadConfig();
+					Messaging.send(sender, "Reloaded CommandNPC");
+					return true;
+				}else{
+					Messaging.sendError(sender, "You do not have permission for this!");
+				}
+			}else{
+				Messaging.sendError(sender, "Wrong Args. Use /commandnpc reload");
+			}
+		}else{
+			Messaging.sendError(sender, "Wrong Args. Use /commandnpc reload");
 		}
 		return false;
 	}
