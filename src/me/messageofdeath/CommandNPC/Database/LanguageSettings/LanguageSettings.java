529ca9fc824f2861e6d99b348e93aae6520e69cd
BuildTools <unconfigured@null.spigotmc.org>
2017-05-18 04:56:20 -0500

    Just some reorganization of the file.

--- src/me/messageofdeath/CommandNPC/Database/LanguageSettings/LanguageSettings.java
+++ src/me/messageofdeath/CommandNPC/Database/LanguageSettings/LanguageSettings.java
@@ -6,6 +6,11 @@ public enum LanguageSettings {
 	General_PluginTag("&8[&2CommandNPC&8] &6"), 
 	General_ErrorTag("&0[&cError&0] &c"), 
 	
+	/**-------------------------------------- CommandNPC NPC --------------------------------------**/
+	CmdNPC_Cooldown("You cannot do this yet!"),
+	CmdNPC_NoMoney("You do not have enough money for this!"),
+	CmdNPC_Executed("Command Executed."),
+	
 	/**-------------------------------------- General Commands --------------------------------------**/
 	Commands_NoPermission("You do not have permission for this!"), 
 	Commands_WrongArgs("Wrong Args. Use /commandnpc reload"),
@@ -21,11 +26,6 @@ public enum LanguageSettings {
 	Commands_SetTo_Header("You have set the following variables:"),
 	Commands_SetTo_Line("  &8- &6%variable to&8: &b%value"),
 	Commands_SetTo_Nothing("  &8- &4Nothing's changed..."),
-	
-	/**-------------------------------------- CommandNPC NPC --------------------------------------**/
-	CmdNPC_Cooldown("You cannot do this yet!"),
-	CmdNPC_NoMoney("You do not have enough money for this!"),
-	CmdNPC_Executed("Command Executed."),
 	
 	/**-------------------------------------- CommandNPC Commands --------------------------------------**/
 	Commands_CmdNPC_Reload("You have successfully reloaded the CommandNPC configuration file!"),
